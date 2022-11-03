897c00f6515b491d1314d16a694fbfca4dee2808
zzmudzio <zzmudzio@gmail.com>
2022-11-03 19:29:10 +0100

    code readibility improvement

--- CPS-tests/src/test/java/CPSSettingsTest.java
+++ CPS-tests/src/test/java/CPSSettingsTest.java
@@ -25,26 +25,21 @@ public class CPSSettingsTest {
 
     @Test(priority = 0)
     public void testSettingsPageOpeningProperly() {
-
         System.out.println("[Test] Weryfikacja poprawnego otwarcia strony konfiguracji portalu.");
         testingObject.goToMainPage();
         Assert.assertTrue(testingObject.goToSettings());
-
     }
 
     @Test(priority = 1)
     public void testAddConnectionButton() {
-
         System.out.println("[Test] Weryfikacja poprawnej pracy przycisku \'Dodaj połączenie\'.");
         testingObject.goToMainPage();
         Assert.assertTrue(testingObject.clickAddConnection());
-
     }
 
     /* at this moment, there is a possibility for testing just a single db connection */
     @Test(priority = 2)
     public void testAddNewConnection() {
-
         String[][] dataArray = CsvReader.readDbConnectionData();
         if(dataArray.length < 2) {
             Assert.assertTrue(false);
@@ -56,31 +51,26 @@ public class CPSSettingsTest {
 
     @Test(priority = 3)
     public void testValidConnection() {
-
         System.out.println("[Test] Weryfikacja poprawnej pracy przycisku \'Test połączenia\'.");
         Assert.assertTrue(testingObject.clickTestConnection());
     }
 
     @Test(priority = 4)
     public void testSaveConnection() {
-
         System.out.println("[Test] Weryfikacja poprawnej pracy przycisku \'Zapisz\'.");
         Assert.assertTrue(testingObject.clickSaveConnection());
     }
 
     @Test(priority = 5)
     public void testCloseConnectionWindow() {
-
         System.out.println("[Test] Weryfikacja poprawnej pracy przycisku \'Zamknij\'.");
         Assert.assertTrue(testingObject.clickCloseConnectionWindow());
     }
 
     @Test(priority = 6)
     public void testDbConnectionRecordVisibility() {
-
         System.out.println("[Test] Weryfikacja istnienia zdefiniowanego połączenia do bazy danych.");
         Assert.assertTrue(testingObject.verifyConnectionExistence());
-
     }
 
     @AfterTest(enabled = false)
