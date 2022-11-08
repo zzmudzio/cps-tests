87fe968a98c1001c8027e88a6fad589efffffc19
zzmudzio <zzmudzio@gmail.com>
2022-11-08 14:18:13 +0100

    Added new class CpsTests

---
+++ CPS-tests/src/test/java/CpsTests.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CpsTests {
+    protected WebDriver driver;
+    protected WebDriverWait driverWait;
+
+}
